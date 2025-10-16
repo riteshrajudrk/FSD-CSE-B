7549f3234695183349228897be80f004321c6c46
Ritesh Kumar <riteshrajudrk@gmail.com>
2025-10-16 15:12:39 +0530

    react app added

---
+++ REACT-APP-NPM/index.jsx
@@ -0,0 +1,12 @@
+import './style.css';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+
+
+
+
+
+
+const parent = document.getElementById("root");
+const root = ReactDOM.createRoot(parent);
+root.render(<App />);
